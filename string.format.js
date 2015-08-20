78d483041668310044245dbd93b821235714d46e
yodfz <yodfz@qq.com>
2015-08-20 14:55:59 +0800

    字符串格式化代码

---
+++ string.format.js
@@ -0,0 +1,17 @@
+/**
+ * Created by 逸风 on 2015/8/20.
+ * Description:字符串格式化
+ * Demo:
+ * stringformat("{0} {1}","hello","world");
+   hello world
+ */
+
+function stringformat(_t) {
+    var args = Array.prototype.slice.call(arguments, 1);
+    return _t.replace(/{(\d+)}/g, function (match, number) {
+        return typeof args[number] != 'undefined'
+            ? args[number]
+            : match
+            ;
+    });
+}
