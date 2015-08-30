d57d18f853d76187c18022a487015cce7fd05914
yodfz <yodfz@qq.com>
2015-08-30 22:32:23 +0800

    update JS

--- getDataByFilter.js
+++ getDataByFilter.js
@@ -10,12 +10,13 @@
  {"sku":1045280612,"material":"意大利头层牛皮/海绵/聚酯纤维/硬木","color":"浓咖啡色","specification":"长72.5X宽52X高46.5CM","salesstates":"ZS","tooltips":"意大利头层牛皮-浓咖啡色"},
  {"sku":1045280611,"material":"意大利头层牛皮/海绵/聚酯纤维/硬木","color":"深栗色","specification":"长72.5X宽52X高46.5CM","salesstates":"Z1","tooltips":"意大利头层牛皮-深栗色"}
  ];
- console.log(Check(_d[0],_c));
- console.log(Check(_d[1],_c));
- console.log(Check(_d[2],_c));
- console.log(Check(_d[3],_c));
+ console.log(checkDataByFilter(_d[0],_c));
+ console.log(checkDataByFilter(_d[1],_c));
+ console.log(checkDataByFilter(_d[2],_c));
+ console.log(checkDataByFilter(_d[3],_c));
  */
-function getDataByFilter(/*待识别数据*/data,/*识别条件*/ condition) {
+function checkDataByFilter(/*待识别数据*/data,/*识别条件*/ condition) {
+    if(condition[key].constructor==undefined) return ;
     //最终判断结果
     var _result = false;
     //用于定义是否有条件，无条件直接略过
@@ -52,4 +53,8 @@ function getDataByFilter(/*待识别数据*/data,/*识别条件*/ condition) {
     }
     _type = null;
     return _start ? true : _result;
+}
+
+function getDataByFilter(/*待识别数据*/data,/*识别条件*/ condition){
+
 }
