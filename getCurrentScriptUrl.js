ad0730f39dc63827af2a0610204993fb8afffb4a
yodfz <yodfz@qq.com>
2015-10-21 18:07:11 +0800

    你增getcurrentscripturl

---
+++ getCurrentScriptUrl.js
@@ -0,0 +1,29 @@
+/**
+ * 获取当前JS的调用URL地址
+  * @returns {*}
+ * @private
+ */
+var _getCurrentScriptUrl = function() {
+    var jsPath, scripts, i;
+    if (_document.currentScript && (jsPath = _document.currentScript.src)) {
+      return jsPath;
+    }
+    scripts = _document.getElementsByTagName("script");
+    if (scripts.length === 1) {
+      return scripts[0].src || undefined;
+    }
+    if ("readyState" in scripts[0]) {
+      for (i = scripts.length; i--; ) {
+        if (scripts[i].readyState === "interactive" && (jsPath = scripts[i].src)) {
+          return jsPath;
+        }
+      }
+    }
+    if (_document.readyState === "loading" && (jsPath = scripts[scripts.length - 1].src)) {
+      return jsPath;
+    }
+    if (jsPath = _getCurrentScriptUrlFromError()) {
+      return jsPath;
+    }
+    return undefined;
+  };
