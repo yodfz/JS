3f94a903a7224b1eb399449c3f15cb3590c1c304
yodfz <yodfz@qq.com>
2015-10-27 17:28:48 +0800

    BUG修复外加新增上传文件后缀限制

--- Src/jQuery.uploadfile.js/jQuery.uploadfile.js
+++ Src/jQuery.uploadfile.js/jQuery.uploadfile.js
@@ -14,19 +14,22 @@
 }(this,function(_c){
     var $fn;
     var $config={
+        //上传地址
         uploadSrc:"",
-        success:null
+        //回调函数
+        success:null,
+        //后缀限制
+        suffix:"gif,png,jpeg,jpg"
     },_config={},$id;
     var _clickObj=$(this);
     var $result="";
 
-    //var $postForm/*提交上传的表单*/;
     $fn=function(_){
         var _that=this;
         if(_!=undefined){
             _config= $.extend({},$config,_);
         }
-        //创建一个form表单
+        //创建一个唯一ID
         $id="uploadfile" + (new Date()).valueOf() + Math.random().toFixed(3).substr(2);
         var _form=document.createElement("form");
         var _iframe=document.createElement("iframe");
@@ -37,7 +40,6 @@
         _form.target="iframe" + $id;
         _iframe.id="iframe" + $id;
         _iframe.name="iframe" + $id;
-
         _file.type="file";
         _file.id="file" + $id;
         _form.appendChild(_iframe);
@@ -46,7 +48,19 @@
         //挂接事件
         $("#iframe" + $id).load(_that.loaded);
         $("#file" + $id).change(function(){
-            $("#" + $id).submit();
+            var _path=$(this).val().split('.');
+            _path=_path[_path.length-1];
+            var _checksuffix=false;
+            for(var _i= 0,_item;_item=_config.suffix[_i++];){
+                if(_item.toLowerCase()==_path.toLowerCase()){
+                    _checksuffix=true;
+                }
+            }
+            if(_checksuffix){
+                $("#" + $id).submit();
+            }else{
+                alert("无法上传此文件,仅限以下后缀文件:" + _config.suffix);
+            }
         });
         _clickObj.click(function(){
             _that.upload();
@@ -54,25 +68,22 @@
         return this;
     };
 
-    //进行上传操作
+    /**
+     * 进行上传操作
+     */
     $fn.prototype.upload=function(){
         $("#file" + $id).trigger("click");
     };
 
     /**
      * 上传完成之后的回调
-     * @param _d
      */
     $fn.prototype.loaded=function(){
         var _body=$(this).contents().find("body");
         var _pre=_body.find("pre");
-        var _result=_pre.html()==""?_body.html():_pre.html();
-        $result=_result;
+        $result=_pre.html()==""?_body.html():_pre.html();
         _config.success(JSON.parse($result));
-        return _result;
+        return $result;
     };
-
-
-
     return new $fn(_c);
 }));
