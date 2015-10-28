993a32981e127c24c61de150f3b6cb0af63854da
yodfz <yodfz@qq.com>
2015-10-28 15:37:00 +0800

    加入了数组限制，仅限最后一个命名空间为数组 如 namespace.time.test[0].a

--- Src/form2JSON.js/jQuery.form2JSON.js
+++ Src/form2JSON.js/jQuery.form2JSON.js
@@ -25,9 +25,21 @@
             var _namespace=_n.split('.');
             _n=_namespace[_namespace.length-1];
             for(var _k= 0;_k<_namespace.length-1;_k++){
-                if(_d[_namespace[_k]]==undefined) _d[_namespace[_k]]={};
+                if(_d[_namespace[_k]]==undefined){
+                    if(_namespace[_k].substr(-1)=="]"){
+                        _d[_namespace[_k]]=[];
+                    }else{
+                        _d[_namespace[_k]]={};
+                    }
+                }
                 _d=_d[_namespace[_k]];
             }
+
+            if(_d instanceof  Array){
+                var _index=_namespace[_namespace.length-2].match(/\[(\d+)\]/ig)?RegExp.$1:0;
+                _d=_d[_index];
+            }
+            //else{
             if(_d[_n]==undefined){
                 _d[_n]=_v;
             }
@@ -42,6 +54,7 @@
                     _d[_n].push(_v);
                 }
             }
+            //}
         }
         return _d;
     };
