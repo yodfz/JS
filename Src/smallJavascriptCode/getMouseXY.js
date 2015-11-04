c48194ba863c250587ba3e016cba715538f0eedd
yodfz <yodfz@qq.com>
2015-11-04 16:04:58 +0800

    代码归类，新增获取鼠标XY的代码

---
+++ Src/smallJavascriptCode/getMouseXY.js
@@ -0,0 +1,21 @@
+/**
+ * 获取当前鼠标XY坐标
+ * @param e
+ * @returns {{x: *, y: *}}
+ */
+function getXY(e) {
+    //用于扩展JQ的触摸事件
+    var $x, $y;
+    if (e.originalEvent.changedTouches) {
+        $x = e.originalEvent.changedTouches[0].pageX;
+        $y = e.originalEvent.changedTouches[0].pageY;
+    } else if (e.changedTouches) {
+        $x = e.changedTouches[0].pageX;
+        $y = e.changedTouches[0].pageY;
+    }
+    else {
+        $x = e.pageX;
+        $y = e.pageY;
+    }
+    return { x: $x, y: $y };
+}
