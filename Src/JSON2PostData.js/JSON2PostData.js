1198ccc5139d22424e22ea5519b856ad856ef817
yodfz <yodfz@qq.com>
2015-11-05 17:24:33 +0800

    类型判断有问题，另外添加了三条测试数据以便展示如何使用

--- Src/JSON2PostData.js/JSON2PostData.js
+++ Src/JSON2PostData.js/JSON2PostData.js
@@ -15,8 +15,10 @@ function JSON2PostData(obj,_key){
         }
     }
     function checkOBJ(_){
-        for(var _key in _){
-            return false;
+        if(typeof _=="object"){
+            for(var $$key in _){
+                return false;
+            }
         }
         return true;
     }
@@ -43,3 +45,7 @@ function JSON2PostData(obj,_key){
     }
     return jsonResult.join("&")
 }
+
+JSON2PostData({"id":"","type":"7"});
+JSON2PostData({ a: { b:1,c:2 }, d: [3,4,{ e:5 }] });
+JSON2PostData({"contents":[{"id":"","type":"7","data":"{\"text\":\"阿萨德\"}"},{"id":"","type":"1","data":"{\"text\":\"请在此输入文字内容!\"}"}],"images":[{"type":1,"imgKey":"qn|xaya|Fs3_GpHAEuEhZx9D3i37rIu6M1_v","imgUrl":"http://xaya.qiniudn.com/Fs3_GpHAEuEhZx9D3i37rIu6M1_v?imageView2/2/w/480/q/100"}],"tags":[{"id":42,"name":"阿斯蒂芬"}],"title":"阿斯顿发斯蒂芬","author":"","categoryId":"3","summary":"阿斯顿发生非打死打法是否打算打发斯蒂芬","id":"","type":"1"})
