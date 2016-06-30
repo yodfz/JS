9fd8467614f18db1b7ed5d7a9fc339a085e2566e
yodfz <yodfz@qq.com>
2016-06-30 16:10:56 +0800

    scroll recoder

--- Src/scrollRecorder/scrollRecorder.js
+++ Src/scrollRecorder/scrollRecorder.js
@@ -1,18 +1,18 @@
 (function (_win, _doc) {
-	var _onscroll = window.onscroll;
-	var _onload = window.onload;
-	window.onscroll = function () {
+	var _onscroll = _win.onscroll;
+	var _onload = _win.onload;
+	_win.onscroll = function () {
 		_onscroll.call(this, arguments);
-		window.sessionStorage.setItem(window.localtion.href.toLowerCase(), window.scrollY);
+		_win.sessionStorage.setItem(_win.localtion.href.toLowerCase(), _win.scrollY);
 		console.log('new scroll');
 	};
-	window.onload = function () {
+	_win.onload = function () {
 		_onload.call(this, arguments);
 		// 读取当前URL标志位
-		var _recorder = window.sessionStorage.getItem(window.localtion.href.toLowerCase());
+		var _recorder = _win.sessionStorage.getItem(_win.localtion.href.toLowerCase());
 		if (_recorder) {
 			//设置位置
-			// window.scrollTo(0,1800)
+			_win.scrollTo(0,_recorder)
 		}
 	};
 })(window, document);
