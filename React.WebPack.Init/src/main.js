7012aa1cf2943b40f606ff348ec9a01a3d0fe12c
yodfz <yodfz@qq.com>
2016-07-01 17:29:43 +0800

    REACT INIT

---
+++ React.WebPack.Init/src/main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (<h1>hello world</h1>);
+  };
+}
+
+// const App = () => <h1>hello world,const!</h1>;
+
+export default App;
