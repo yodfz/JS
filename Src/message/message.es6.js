d7c5f5e29a07a1e0c4c350fbcec4b876de1231fd
yodfz <yodfz@qq.com>
2016-08-10 14:57:57 +0800

    fix bug

--- Src/message/message.es6.js
+++ Src/message/message.es6.js
@@ -1,6 +1,6 @@
 let template = `
-    <div id="message_{{id}}" class="screenLock animated fadeIn">
-    <div class="message">
+    <div id="message_{{id}}" class="screenLock">
+    <div id="message_js_{{id}}" class="message animated {{animation}}In">
         <div class="title">
         {{title}}
         </div>
@@ -13,16 +13,30 @@ let template = `
     </div></div>`;
 
 export default {
-	showMsg (_title, _content, okEvent, cancelEvent, _type) {
+	showMsg (_title, _content, okEvent, cancelEvent, _type, _animation) {
 		let _id = +new Date();
-		let $hasTouch = 'ontouchstart' in window;
-		// 点透问题
-		let $eventStart = $hasTouch ? 'touchstart' : 'click';
-		let _html = template.replace('{{title}}', _title)
-			.replace('{{content}}', _content)
-			.replace('{{id}}', _id);
+		let d = document;
+		if (typeof _title === 'object') {
+			let result = Object.assign({}, _title);
+			_title = result.title;
+			_content = result.content;
+			okEvent = result.okEvent;
+			cancelEvent = result.cancelEvent;
+			_type = result.type;
+			_animation = result.animation;
+		}
 		// 判断类型,计算给什么种类的按钮
 		_type = _type || 0;
+		_animation = _animation || 'fade';
+
+		let $hasTouch = 'ontouchend' in window;
+		// 点透问题
+		let $eventStart = $hasTouch ? 'touchend' : 'click';
+		let _html = template.replace('{{title}}', _title)
+			.replace('{{content}}', _content)
+			.replace(/\{\{id\}\}/ig, _id)
+			.replace('{{animation}}', _animation);
+
 		let _btnHtml = [
 			`<button class="leftBtn js-cancel">取消</button>
              <button class="rightBtn js-ok">确定</button>`,
@@ -30,25 +44,38 @@ export default {
             确定</button>`
 		];
 		_html = _html.replace('{{button}}', _btnHtml[_type]);
-		document.body.insertAdjacentHTML('beforeend', _html);
-		let _obj = document.querySelector('#message_' + _id);
-		document.querySelector('.wrapperContains').className = 'wrapperContains blur';
+		d.body.insertAdjacentHTML('beforeend', _html);
+		let _obj = d.querySelector('#message_' + _id);
+		// let _objJS = d.querySelector('#message_js_' + _id);
+		d.querySelector('.wrapperContains').className = 'wrapperContains blur';
 		_obj.addEventListener($eventStart, function (e) {
+			e.preventDefault();
 			let _className = e.target.className;
 			if (_className.indexOf('js-cancel') > -1) {
-				e.stopPropagation();
-				e.preventDefault();
-				document.querySelector('.wrapperContains').className = 'wrapperContains';
+				d.querySelector('.wrapperContains').className = 'wrapperContains';
 				cancelEvent && cancelEvent();
-				_obj.remove();
+				// _obj.remove();
+				// _obj && _obj.parentNode.removeChild(_obj);
+				//
+				_obj.className = `screenLock animated ${_animation}Out`;
+				setTimeout(function () {
+					_obj && _obj.parentNode.removeChild(_obj);
+				}, 500);
 			}
 			if (_className.indexOf('js-ok') > -1) {
-				e.stopPropagation();
-				e.preventDefault();
-				document.querySelector('.wrapperContains').className = 'wrapperContains';
+				d.querySelector('.wrapperContains').className = 'wrapperContains';
 				okEvent && okEvent();
-				_obj.remove();
+				// _obj.remove();
+				// _obj && _obj.parentNode.removeChild(_obj);
+				// _obj && _obj.parentNode.removeChild(_obj);
+				_obj.className = `screenLock animated ${_animation}Out`;
+				setTimeout(function () {
+					_obj && _obj.parentNode.removeChild(_obj);
+				}, 500);
 			}
+		});
+		_obj.addEventListener('click', function (e) {
+			e.preventDefault();
 		});
 	}
 };
