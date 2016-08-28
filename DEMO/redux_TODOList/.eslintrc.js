85a3994c287a89f6e8934bb8ef594eec8901d41c
yodfz <yodfz@qq.com>
2016-08-28 10:57:53 +0800

    redux todo list init

---
+++ DEMO/redux_TODOList/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    root: true,
+    parserOptions: {
+        sourceType: 'module'
+    },
+    // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
+    extends: 'standard',
+    // required to lint *.vue files
+    plugins: [
+        'html'
+    ],
+    // add your custom rules here
+    'rules': {
+        // allow paren-less arrow functions
+        'arrow-parens': 0,
+        'semi': [2, "always"],
+        // allow debugger during development
+        'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+        "no-spaced-func": 2,
+        "arrow-spacing": 0,
+        "space-before-function-paren": [0, "always"],//函数定义时括号前面要不要有空格
+        "indent": [2, 4]
+    }
+};
